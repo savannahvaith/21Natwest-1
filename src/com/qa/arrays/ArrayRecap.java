7a4aab530abe2ebfe2a782066b80430507d2ee00
savannahvaith <sav.vaithilingam@gmail.com>
2021-06-30 14:35:47 +0100

    SV: Multi dimensional arrays

--- src/com/qa/arrays/ArrayRecap.java
+++ src/com/qa/arrays/ArrayRecap.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ArrayRecap {
 
 	static List<String> names = new ArrayList<>();
-	
+
 	public static void main(String[] args) {
 
 		// ArrayList - A way to store information
@@ -18,25 +18,50 @@ public class ArrayRecap {
 		names.add("Mike");
 		names.add("lisa");
 		names.add("kim");
-		
+
 		System.out.println(names.size());
 //		printNames();
-		enhancedPrintNames();
+//		enhancedPrintNames();
+		printMDArray();
 	}
-	
+
 	// print arrays
 	public static void printNames() {
-		for(int i=0; i<names.size(); i++) {
+		for (int i = 0; i < names.size(); i++) {
 			System.out.println(names.get(i));
 		}
 	}
-	
+
 	// enhanced for loop - let the computer do the hard stuff
-	
 	public static void enhancedPrintNames() {
-		for(String person : names) {
+		for (String person : names) {
 			System.out.println(person);
 		}
 	}
-	
+
+	// Multi-dimensional arrays
+
+	public static int[][] ageArray = { { 1990, 25, 45 }, { 1995, 26, 54 } };
+
+	public static void printMDArray() {
+//		System.out.println(ageArray[0][3]);
+
+		// loop through the COLUMNS
+		for (int i = 0; i < ageArray.length; i++) {
+			// loop through the ROWS
+			for (int j = 0; j < ageArray[i].length; j++) {
+				System.out.println(ageArray[i][j]);
+
+//				ageArray[0][0]
+//				ageArray[0][1]
+//				ageArray[0][2]
+
+//				ageArray[1][0]
+//				ageArray[1][1]
+//				ageArray[1][2]
+			}
+		}
+
+	}
+
 }
